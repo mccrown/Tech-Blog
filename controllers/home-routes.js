e295f1bd5682cf2ab3fe188ae18d1f4c7666a70c
CMcCown <connermccown94@gmail.com>
2022-05-23 11:21:05 -0600

    created handlebars homepage

---
+++ controllers/home-routes.js
@@ -0,0 +1,13 @@
+
+//have all teh routes such as homepage and login
+
+const router = require('express').Router()
+const sequelize = require('../config/connection')
+//const models
+
+
+router.get('/', (req, res) => {
+    res.render('homepage')
+})
+
+module.exports = router; 
